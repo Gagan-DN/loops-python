f0861e94538981d20d216db2e232ffcfcebde90e
Gagan-DN <116274483+Gagan-DN@users.noreply.github.com>
2024-01-06 22:36:15 +0530

    Create continue_break.py
    an example for continue and break statement

---
+++ continue_break.py
@@ -0,0 +1,8 @@
+#loops(break,continue)
+#1 to ask the user for a number while in range (1 TO 10)
+num=1
+while(1<=num<=10):
+    num=int(input("Enter a number:"))
+    print("number:",num)
+    if num>10:
+        break
